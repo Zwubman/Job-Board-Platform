6f413018f28339363fbdf6f533e5b16261bee792
Zwubman <zwubeman@gmail.com>
2025-04-08 05:34:20 -0400

    create user model

---
+++ Models/userModel.js
@@ -0,0 +1,72 @@
+import mongoose, { Schema } from "mongoose";
+
+const usersSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    middleName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["Employer", "User"],
+      default: "User",
+      required: true,
+    },
+    bio: {
+      type: String,
+    },
+
+    experience: [
+      {
+        title: String,
+        company: String,
+        startDate: Date,
+        endDate: Date,
+        description: String,
+      },
+    ],
+
+    education: [
+      {
+        degree: String,
+        institution: String,
+        startYear: Number,
+        endYear: Number,
+      },
+    ],
+
+    skills: [String],
+
+    companyName: {
+      type: String,
+    },
+
+    companyDescription: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", usersSchema);
