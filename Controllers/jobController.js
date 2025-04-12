3e124a2dbee59513cfc1ba99601dc13137bd4612
Zwubman <zwubeman@gmail.com>
2025-04-12 04:43:31 -0400

    add function to retrieve all created job

--- Controllers/jobController.js
+++ Controllers/jobController.js
@@ -46,7 +46,7 @@ export const createNewJob = async (req, res) => {
       return res.status(401).json({ message: "Job already created.", job });
     }
 
-    const job = await new Job({
+    const job = new Job({
       title,
       description,
       location,
@@ -55,6 +55,7 @@ export const createNewJob = async (req, res) => {
       jobType,
       category,
       applicationDeadline,
+      employer: userId,
     });
 
     await job.save();
@@ -63,5 +64,30 @@ export const createNewJob = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Fail to create new job.", error });
+  }
+};
+
+// Retrieve all jobs
+export const getAllJob = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    const jobs = await Job.find();
+    if (!jobs || jobs.length === 0) {
+      return res.status(404).json({ message: "There are no jobs." });
+    }
+
+    res.status(200).json({
+      message: "All jobs retrieved successfully",
+      jobs: jobs,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to retrieve jobs.", error });
   }
 };
--- Routes/jobRoute.js
+++ Routes/jobRoute.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 import express from "express";
 import { verifyToken } from "../Middlewares/authMiddleware.js";
-import { createNewJob } from "../Controllers/jobController.js";
+import { createNewJob, getAllJob } from "../Controllers/jobController.js";
 
 const jobRoute = express.Router();
 
 jobRoute.post("/create-job", verifyToken, createNewJob);
-
-
+jobRoute.get("/all-jobs", verifyToken, getAllJob);
 
 export default jobRoute;
